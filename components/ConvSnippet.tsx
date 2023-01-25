c591b6c9f61a37669e13ac5d7a594c03bde0ea78
Xe Iaso <xe@tailscale.com>
2023-01-25 19:59:55 +0000

    prettier
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- components/ConvSnippet.tsx
+++ components/ConvSnippet.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
-import { allAuthors, Authors as Author } from "contentlayer/generated";
-import Image from "next/image";
+import { ReactNode } from 'react';
+import { allAuthors, Authors as Author } from 'contentlayer/generated';
+import Image from 'next/image';
 
 export interface ConvSnippetProps {
   name: string;
@@ -19,19 +19,14 @@ const findAuthor = (name: string): Author => {
 
 export const ConvSnippet = ({ name, children }: ConvSnippetProps) => {
   const author = findAuthor(name);
-  const handle = author.name.split(" ", 1)[0];
+  const handle = author.name.split(' ', 1)[0];
 
   return (
-    <div className="p-4 flex space-x-4 bg-gray-100 rounded-lg">
-      <div className="w-16 h-16 shrink-0 rounded-lg overflow-hidden bg-gray-200 flex items-center justify-center self-center">
-        <Image
-          src={author.avatar}
-          width={64}
-          height={64}
-          alt={`the avatar for ${author.name}`}
-        />
+    <div className="flex space-x-4 rounded-lg bg-gray-100 p-4">
+      <div className="flex h-16 w-16 shrink-0 items-center justify-center self-center overflow-hidden rounded-lg bg-gray-200">
+        <Image src={author.avatar} width={64} height={64} alt={`the avatar for ${author.name}`} />
       </div>
-      <div className="self-center min-w-0">
+      <div className="min-w-0 self-center">
         &lt;<b>{handle}</b>&gt; {children}
       </div>
     </div>
