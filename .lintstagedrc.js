6d151d03a3497dff5c3d3ca3a8050576fe574294
Tyler Smalley <tyler@tailscale.com>
2023-01-27 14:29:03 -0800

    Pass author to ConvSnippet component (#19)
    Having the compent fetch the data was resulting in a hydration error.
    Since we have the data already, I think it makes the most sense to fetch
    pass it to the component.
    
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,4 +7,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*': 'prettier -w',
 };
--- components/ConvSnippet.tsx
+++ components/ConvSnippet.tsx
@@ -1,25 +1,19 @@
 import { ReactNode } from 'react';
-import { allAuthors, Authors as Author } from 'contentlayer/generated';
+import { Authors as Author } from 'contentlayer/generated';
 import Image from 'next/image';
 
 export interface ConvSnippetProps {
+  authors: Author[];
   name: string;
   children: ReactNode;
 }
 
-const findAuthor = (name: string): Author => {
-  for (const author of allAuthors) {
-    if (author.name == name) {
-      return author;
-    }
+export const ConvSnippet = ({ authors, name, children }: ConvSnippetProps) => {
+  const author = authors.find((author) => author.name === name);
+
+  if (!author) {
+    throw new Error(`Author ${name} not found!`);
   }
-
-  return allAuthors[0];
-};
-
-export const ConvSnippet = ({ name, children }: ConvSnippetProps) => {
-  const author = findAuthor(name);
-  const handle = author.name.split(' ', 1)[0];
 
   return (
     <div className="flex space-x-4 rounded-lg bg-gray-100 p-4 dark:bg-gray-800">
@@ -27,7 +21,7 @@ export const ConvSnippet = ({ name, children }: ConvSnippetProps) => {
         <Image src={author.avatar} width={64} height={64} alt={`the avatar for ${author.name}`} />
       </div>
       <div className="min-w-0 self-center">
-        &lt;<b>{handle}</b>&gt; {children}
+        &lt;<b>{author.handle}</b>&gt; {children}
       </div>
     </div>
   );
