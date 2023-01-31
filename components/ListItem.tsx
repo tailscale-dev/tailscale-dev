9a0d8dfa4a40a336a2d4c86f876df4a66e84f397
Tyler Smalley <tyler@tailscale.com>
2023-01-31 09:32:16 -0800

    Improve readability and a11y of list items
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -15,15 +15,13 @@ export function ListItem({ slug, title, date, location, path, tags = [], summary
   return (
     <li key={slug} className="py-6">
       <article>
-        <div className="space-y-6">
+        <div className="space-y-6 text-gray-900 dark:text-white">
           <div>
             <h2 className="text-2xl font-bold leading-8 tracking-tight">
-              <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                {title}
-              </Link>
+              <Link href={`/${path}`}>{title}</Link>
             </h2>
 
-            <div className="font-mono text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+            <div className="font-mono text-base font-medium leading-6">
               <span className="pr-4">&frasl;&frasl;</span>
               <time dateTime={date}>{date}</time>
               {tags && tags.length > 0 && (
@@ -44,7 +42,7 @@ export function ListItem({ slug, title, date, location, path, tags = [], summary
               )}
             </div>
           </div>
-          <div className="prose max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
+          <div className="prose max-w-none">{summary}</div>
         </div>
       </article>
     </li>
