d02dbb000a0dc5c0e80b354d935c42edf46e2180
Tyler Smalley <tyler@tailscale.com>
2023-02-03 12:53:01 -0800

    Adds RudderStack integration for analytics (#66)
    Closes #65
    
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,15 @@ class MyDocument extends Document {
       <Html lang={siteMetadata.language} className="scroll-smooth">
         <Head>
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId","getUserId","getUserTraits","getGroupId","getGroupTraits","startSession","endSession","getSessionId"],e.factory=function(t){return function(){var r=Array.prototype.slice.call(arguments);return r.unshift(t),e.push(r),e}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.rudderlabs.com/v1.1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
+              e.load("${process.env.NEXT_PUBLIC_RUDDERSTACK_KEY}","${process.env.NEXT_PUBLIC_RUDDERSTACK_URL}"),
+              e.page()}();
+          `,
+            }}
+          />
         </Head>
         <body className="text-black min-h-screen bg-white antialiased dark:bg-gray-900 dark:text-white">
           <Main />
