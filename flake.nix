276f75fb7f3be779d9829407c2196c832ce42465
Xe Iaso <xe@tailscale.com>
2023-02-03 14:27:37 +0000

    flake.nix: comment up my dirty hack
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,16 @@
   let upstream = nixpkgs.legacyPackages.${system};
 
   withGo120 = pkgs: upstream.extend (final: prev: rec {
-    # Override go_1_20 so that all go binaries are built with tailscale's go toolchain
+    # Override go_1_19 so that go-tools, gotools, and gopls are built with Go 1.20
+    #
+    # Listen here to my tale of woe: if these tools are built with an older version
+    # of the go compiler toolchain, this will cause them to get angy when Tailscale
+    # code inevitably uses Go 1.20 features out of an understandable desire to have
+    # nice things. As of the time of writing this comment, there is no good way to
+    # override the version of Go that `buildGo119Module` (the main entrypoint to
+    # build Go 1.19 modules in Nix-land) uses. This is a HACK and I hate it but
+    # this will in fact work because buildGo119Module pulls from the `go_1_19`
+    # package.
     go_1_19 = pkgs.go_1_20;
   });
 
@@ -22,7 +31,7 @@
         yarn
 
         # go tools
-        go
+        go_1_20
         go-tools
         gotools
         gopls
