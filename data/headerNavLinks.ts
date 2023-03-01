31e38bca35bef1be3a8a1ea360a18c53715f8811
Xe Iaso <xe@tailscale.com>
2023-03-01 17:59:13 +0000

    data/headerNavLinks: link to solutions page
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 export const headerNavLinks = [
   { href: '/events', title: 'Events' },
   { href: '/blog', title: 'Blog' },
+  { href: '/solutions', title: 'Solutions' },
   { href: 'https://tailscale.com/kb/', title: 'Docs' },
 ];
