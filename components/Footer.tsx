c4cb7fa138d4e328496c7f2b9ac3203409df6733
Tyler Smalley <tyler@tailscale.com>
2023-03-02 14:56:20 -0800

    Adds brand disclosure to footer
    Closes #28
    
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,13 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{`© ${new Date().getFullYear()}`} Tailscale Inc.</div>
+        <div className="text-xs text-gray-600">
+          <p className="mb-3">The official community site of Tailscale</p>
+          <p className="mb-3">
+            WireGuard is a registered
+            <br /> trademark of Jason A. Donenfeld.
+          </p>
+          <p className="mb-3">&copy; Tailscale Inc.</p>
         </div>
       </div>
     </footer>
