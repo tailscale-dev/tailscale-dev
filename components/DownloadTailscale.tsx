2bc32adf7fd77b9361e688587be2c1c72b2f5ca8
Xe Iaso <xe@tailscale.com>
2023-03-02 19:11:41 +0000

    components/DownloadTailscale: fix warning
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- components/DownloadTailscale.tsx
+++ components/DownloadTailscale.tsx
@@ -2,7 +2,7 @@ export default function DownloadTailscale() {
   return (
     <p>
       <a href="https://tailscale.com/download" target="_blank" rel="noopener noreferrer">
-        <button class="button button-primary">Download Tailscale</button>
+        <button className="button button-primary">Download Tailscale</button>
       </a>
     </p>
   );
