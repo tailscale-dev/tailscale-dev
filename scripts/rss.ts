6dfff9288c9e27884ed2782bc765c0846e0f394b
shayne <shayne@tailscale.com>
2023-03-09 23:49:39 -0500

    lib/utils: [rss] Update RSS feed. 20 items, desc order, add author (#144)
    Fixes: #139
    
    Signed-off-by: Shayne Sweeney <shayne@tailscale.com>

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -1,9 +1,9 @@
 import { generateRSS } from '../lib/utils/generate-rss';
 import { siteMetadata } from '../data/siteMetadata';
-import { allBlogs } from '../.contentlayer/generated/index.mjs';
+import { allBlogs, allAuthors } from '../.contentlayer/generated/index.mjs';
 
 const rss = () => {
-  generateRSS(siteMetadata, allBlogs);
+  generateRSS(siteMetadata, allBlogs, allAuthors);
   console.log('RSS feed generated...');
 };
 export default rss;
