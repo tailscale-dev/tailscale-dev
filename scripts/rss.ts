f4877d9690013048b66b905790dc16654f2b5e5e
Ross Zurowski <ross@rosszurowski.com>
2023-03-21 11:33:00 -0400

    Use API routes for RSS feed
    This commit updates the RSS feeds to be generated at request time via
    an API route rather than at build time. I find this helps minimize the
    need for "post-build" steps and makes it easier to pull content from
    dynamic sources when working with Next.js.

--- scripts/rss.ts
+++
@@ -1,9 +0,0 @@
-import { generateRSS } from '../lib/utils/generate-rss';
-import { siteMetadata } from '../data/site-metadata';
-import { allBlogs, allAuthors } from '../.contentlayer/generated/index.mjs';
-
-const rss = () => {
-  generateRSS(siteMetadata, allBlogs, allAuthors);
-  console.log('RSS feed generated...');
-};
-export default rss;
