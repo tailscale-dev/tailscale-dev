25ccee8c95697f3297c3f09a2d33e815249c6d6b
Tyler Smalley <tyler@tailscale.com>
2023-03-30 14:41:50 -0700

    [search] Add meta tags for crawler
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- components/seo.tsx
+++ components/seo.tsx
@@ -4,16 +4,15 @@ import { useRouter } from 'next/router';
 import { siteMetadata } from '@/data/site-metadata';
 import { CoreContent } from '@/lib/utils/contentlayer';
 import type { Blog, Authors } from 'contentlayer/generated';
+import { tags } from '@/data/tags';
+
+import type { BlogPosting, ImageObject, Person } from 'schema-dts';
+
 interface CommonSEOProps {
   title: string;
   description: string;
   ogType: string;
-  ogImage:
-    | string
-    | {
-        '@type': string;
-        url: string;
-      }[];
+  ogImage: string | ImageObject[];
   twImage: string;
   canonicalUrl?: string;
 }
@@ -28,32 +27,34 @@ const CommonSEO = ({
 }: CommonSEOProps) => {
   const router = useRouter();
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="robots" content="follow, index" />
-      <meta name="description" content={description} />
-      <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
-      <meta property="og:type" content={ogType} />
-      <meta property="og:site_name" content={siteMetadata.title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
-      {Array.isArray(ogImage) ? (
-        ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
-      ) : (
-        <meta property="og:image" content={ogImage} key={ogImage} />
-      )}
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={description} />
+        <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
+        <meta property="og:type" content={ogType} />
+        <meta property="og:site_name" content={siteMetadata.title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        {Array.isArray(ogImage) ? (
+          ogImage.map(({ url }) => <meta property="og:image" content={`${url}`} key={`${url}`} />)
+        ) : (
+          <meta property="og:image" content={ogImage} key={ogImage} />
+        )}
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@tailscale" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={twImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@tailscale" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={twImage} />
 
-      <link
-        rel="canonical"
-        href={canonicalUrl ? canonicalUrl : `${siteMetadata.siteUrl}${router.asPath}`}
-      />
-    </Head>
+        <link
+          rel="canonical"
+          href={canonicalUrl ? canonicalUrl : `${siteMetadata.siteUrl}${router.asPath}`}
+        />
+      </Head>
+    </>
   );
 };
 
@@ -106,78 +107,86 @@ interface BlogSeoProps extends CoreContent<Blog> {
   url: string;
 }
 
-export const BlogSEO = ({
-  authorDetails,
-  title,
-  summary,
-  date,
-  lastmod,
-  url,
-  images = [],
-  canonicalUrl,
-}: BlogSeoProps) => {
-  const publishedAt = new Date(date).toISOString();
-  const modifiedAt = new Date(lastmod || date).toISOString();
-  const imagesArr =
-    images.length === 0
-      ? [siteMetadata.socialBanner]
-      : typeof images === 'string'
-      ? [images]
-      : images;
+export const BlogSEO = (b: BlogSeoProps) => {
+  const publishedAt = new Date(b.date).toISOString();
+  const modifiedAt = new Date(b.lastmod || b.date).toISOString();
+  let images: string[] = [];
 
-  const featuredImages = imagesArr.map((img) => {
-    return {
-      '@type': 'ImageObject',
-      url: img.includes('http') ? img : siteMetadata.siteUrl + img,
-    };
-  });
-
-  let authorList;
-  if (authorDetails) {
-    authorList = authorDetails.map((author) => {
-      return {
-        '@type': 'Person',
-        name: author.name,
-      };
-    });
+  switch (true) {
+    case typeof b.images === 'string':
+      images = [b.images.includes('http') ? `${b.images}` : siteMetadata.siteUrl + b.images];
+    case Array.isArray(b.images):
+      images = b.images.map((i) => (i.includes('http') ? i : siteMetadata.siteUrl + i));
   }
 
-  const structuredData = {
-    '@context': 'https://schema.org',
-    '@type': 'Article',
+  if (images.length === 0) {
+    images = [siteMetadata.siteUrl + siteMetadata.socialBanner];
+  }
+
+  const ogImages: ImageObject[] = images?.map((url) => ({
+    '@type': 'ImageObject',
+    url,
+  }));
+
+  const ogAuthors: Person[] = b.authorDetails?.map((author) => ({
+    '@type': 'Person',
+    name: author.name,
+  }));
+
+  // https://schema.org/BlogPosting
+  const structuredData: BlogPosting = {
+    '@type': 'BlogPosting',
     mainEntityOfPage: {
       '@type': 'WebPage',
-      '@id': url,
+      '@id': b.url,
     },
-    headline: title,
-    image: featuredImages,
+    headline: b.title,
     datePublished: publishedAt,
     dateModified: modifiedAt,
-    author: authorList,
-    description: summary,
-  };
+    description: b.summary,
 
-  const twImageUrl = featuredImages[0].url;
+    // https://schema.org/ImageObject
+    image: ogImages,
+
+    //https://schema.org/Person
+    author: ogAuthors,
+  };
 
   return (
     <>
       <CommonSEO
-        title={title}
-        description={summary}
+        title={b.title}
+        description={b.summary}
         ogType="article"
-        ogImage={featuredImages}
-        twImage={twImageUrl}
-        canonicalUrl={canonicalUrl}
+        ogImage={ogImages}
+        twImage={images[0]}
+        canonicalUrl={b.canonicalUrl}
       />
       <Head>
-        {date && <meta property="article:published_time" content={publishedAt} />}
-        {lastmod && <meta property="article:modified_time" content={modifiedAt} />}
+        {/* Open Graph */}
+        {/* https://ogp.me/#no_vertical */}
+        {b.date && <meta property="article:published_time" content={publishedAt} />}
+        {b.lastmod && <meta property="article:modified_time" content={modifiedAt} />}
+
+        {/* schema.org */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(structuredData, null, 2),
           }}
         />
+
+        {/* Elastic Site Search */}
+        <meta className="elastic" name="type" content="article" />
+        <meta className="elastic" name="slug" content={b.slug} />
+        <meta className="elastic" name="published_time" content={publishedAt} />
+        {b.tags?.map((tag) => (
+          <meta key={tag} className="elastic" name="tags" content={tags[tag] ? tags[tag] : tag} />
+        ))}
+
+        {b.authorDetails?.map((author) => (
+          <meta key={author.name} className="elastic" name="authors" content={author.name} />
+        ))}
       </Head>
     </>
   );
