de9d97f8b7ebf80fcb44a79b031cef36b69509d5
Shayne Sweeney <shayne@tailscale.com>
2023-04-02 23:27:10 -0400

    fixup! Update components/april-fools-warning.tsx

--- components/april-fools-warning.tsx
+++ components/april-fools-warning.tsx
@@ -1,5 +1,11 @@
-import Warning from "./warning";
+import Warning from './warning';
 
 export default function AprilFoolsWarning() {
-  return <Warning>This is an April Fool{"'"}s day post. This is going to reference things that do not exist yet. Please be warned before attempting to replicate any of the absurd acts mentioned here.</Warning>;
+  return (
+    <Warning>
+      {
+        "This is an April Fool's day post. This is going to reference things that do not exist yet. Please be warned before attempting to replicate any of the absurd acts mentioned here."
+      }
+    </Warning>
+  );
 }
