72910e268c6f56a92f631680aa3c301fd8ad920f
Xe Iaso <xe@tailscale.com>
2023-04-04 15:07:54 -0400

    components/xesite-conv: remove XeDN hack (#178)
    I hacked this up so that it would work more easily. This cleans that up
    so that the assets are served from .dev properly.

--- components/xesite-conv.tsx
+++ components/xesite-conv.tsx
@@ -7,15 +7,27 @@ export interface XesiteConvProps {
   children: ReactNode;
 }
 
+const widths = {
+  aoi: {
+    coffee: 64,
+    facepalm: 64,
+    wut: 64,
+  },
+  mara: {
+    happy: 42,
+    hacker: 49,
+  },
+};
+
 export default function XesiteConv({ name, mood, children }: XesiteConvProps) {
   return (
     <>
       <div className="my-4 flex space-x-4 rounded-md border border-solid border-gray-200 bg-gray-100 p-3 dark:border-gray-700  dark:bg-gray-800">
         <div className="flex h-16 w-16 shrink-0 items-center justify-center self-center overflow-hidden rounded-lg bg-gray-200 dark:bg-gray-700">
           <Image
-            src={`https://cdn.xeiaso.net/sticker/${name.toLowerCase()}/${mood}/128`}
-            width={64}
+            src={`/images/xe/stickers/${name.toLowerCase()}/${mood}.png`}
             height={64}
+            width={widths[name.toLowerCase()][mood]}
             alt={`A picture of the character ${name} in a ${mood} mood.}`}
           />
           <br />
