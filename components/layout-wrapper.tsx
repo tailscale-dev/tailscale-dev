b93218b2bb53715ed53bebd5cf15dccbb2510e26
Xe Iaso <xe@tailscale.com>
2023-08-16 13:05:00 -0400

    components/layout-wrapper: use a ternary for the search bar
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -56,39 +56,38 @@ const LayoutWrapper = ({ children }: Props) => {
                   </li>
                 </>
               ))}
-              {noSearch && (
+              {noSearch ? (
                 <li className="px-1.5 lg:px-2 xl:px-3 font-medium transition-colors duration-200 hover:text-gray-600">
                   <Link href="/search">Search</Link>
                 </li>
+              ) : (
+                <>
+                  <div className="px-1.25 lg:px-2 xl:px-3 pt-8 hidden md:block lg:hidden">
+                    <Link
+                      className="font-medium transition-colors duration-200 hover:text-gray-600"
+                      href="/search"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="h-5 w-5"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </Link>
+                  </div>
+
+                  <div className="px-3 pt-8 w-sm hidden lg:block">
+                    <SearchBar />
+                  </div>
+                </>
               )}
             </ul>
-            {noSearch == false && (
-              <>
-                <div className="px-1.25 lg:px-2 xl:px-3 pt-8 hidden md:block lg:hidden">
-                  <Link
-                    className="font-medium transition-colors duration-200 hover:text-gray-600"
-                    href="/search"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="h-5 w-5"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </Link>
-                </div>
-
-                <div className="px-3 pt-8 w-sm hidden lg:block">
-                  <SearchBar />
-                </div>
-              </>
-            )}
           </div>
           <div className="ml-auto hidden pt-8 md:block">
             <ul className="flex items-center">
