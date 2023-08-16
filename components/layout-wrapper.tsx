48d9643fb74e04b9a3c47e763e2020122994774e
Xe Iaso <xe@tailscale.com>
2023-08-16 13:09:52 -0400

    components/layout-wrapper: make the searchbar fallback look a little better at w=796
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -14,6 +14,21 @@ interface Props {
   children: ReactNode;
 }
 
+const SearchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="h-5 w-5"
+  >
+    <path
+      fillRule="evenodd"
+      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
 // guide: https://nextjs.org/docs/basic-features/font-optimization#with-tailwind-css
 const inter = Inter({
   subsets: ['latin'],
@@ -39,7 +54,7 @@ const LayoutWrapper = ({ children }: Props) => {
                 <span className="pr-2 text-gray-700">~$</span>community
               </span>
             </Link>
-            <ul className="relative hidden pt-8 md:flex">
+            <ul className="relative hidden pt-8 md:flex items-center flex">
               {leftHeaderNavLinks.map((link) => (
                 <>
                   <li key={link.title}>
@@ -58,7 +73,12 @@ const LayoutWrapper = ({ children }: Props) => {
               ))}
               {noSearch ? (
                 <li className="px-1.5 lg:px-2 xl:px-3 font-medium transition-colors duration-200 hover:text-gray-600">
-                  <Link href="/search">Search</Link>
+                  <Link href="/search">
+                    <span className="hidden md:block lg:hidden">
+                      <SearchIcon />
+                    </span>
+                    <span className="hidden lg:block">Search</span>
+                  </Link>
                 </li>
               ) : (
                 <>
@@ -67,18 +87,7 @@ const LayoutWrapper = ({ children }: Props) => {
                       className="font-medium transition-colors duration-200 hover:text-gray-600"
                       href="/search"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-5 w-5"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <SearchIcon />
                     </Link>
                   </div>
 
