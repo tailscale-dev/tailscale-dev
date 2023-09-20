884ac9e5cf5bc443a539fad14a1cd1f74448c2c0
Tyler Smalley <tyler@tailscale.com>
2023-09-20 12:41:57 -0700

    Create stub for Explorables page
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

---
+++ pages/explorables/test.tsx
@@ -0,0 +1,12 @@
+export default function ExplorablesTestPage() {
+  return (
+    <>
+      <header className="bg-gray-900 py-20 text-center text-gray-100">
+        <h1 className="text-4xl font-medium leading-tight tracking-tight">Explorables Test</h1>
+      </header>
+      <main className="container max-w-4xl">
+        <h1>Foo</h1>
+      </main>
+    </>
+  );
+}
